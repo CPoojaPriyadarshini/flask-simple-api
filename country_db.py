da6fdea41ded42966489ba6161a8d5a3e8c2f3bc
CPoojaPriyadarshini <poojachandrabanu@gmail.com>
2018-12-24 10:36:53 +0530

    Add files via upload

---
+++ country_db.py
@@ -0,0 +1,14 @@
+from flask import Flask
+import sqlite3
+app=Flask(__name__)
+
+@app.route('/')
+def ret():
+    receive=sqlite3.connect('D:/Pooja/VSCodes/templates/country.db')
+    receive=receive.execute("SELECT * FROM countryIndia")
+    info=receive.fetchall()
+    for det in info:
+        print(det)
+    return "Done"
+
+app.run()
